3942f86d7d323f1653cb78a3e58a4012b0bcaa29
phungtuhieu <phung.tuhieu@gmail.com>
2023-07-11 12:14:31 +0700

    [FE - Admin] - Tự Hiếu xong header

--- laptech/src/main/java/com/laptech/controller/admin/IndexAdminController.java
+++ laptech/src/main/java/com/laptech/controller/admin/IndexAdminController.java
@@ -1,5 +1,14 @@
 package com.laptech.controller.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
 public class IndexAdminController {
-    
+    @RequestMapping("/index")
+    public String index(Model model) {
+    	return "admin/index-admin";
+    }
 }
