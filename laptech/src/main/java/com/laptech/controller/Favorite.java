318238910abf21f9ceda19483e3a5ca1e0971b97
HuuTaiOne <nguyentaint979@gmail.com>
2023-07-11 15:06:59 +0700

    Hữu Tài - Hoàn thành index,favorite,list-categories

---
+++ laptech/src/main/java/com/laptech/controller/Favorite.java
@@ -0,0 +1,13 @@
+package com.laptech.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Favorite {
+        @RequestMapping("/favorite")
+        public String index(Model model) {
+            return "layout/favorite";
+        }
+}
---
+++ laptech/src/main/java/com/laptech/controller/ListCategory.java
@@ -0,0 +1,26 @@
+package com.laptech.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ListCategory {
+
+        @RequestMapping("/ListCategory/{name}")
+        public String index(@PathVariable("name") String name, Model model) {
+            model.addAttribute("name", name); 
+            System.out.println(name);
+            return "layout/list_categories";
+        }
+
+        @RequestMapping("/ListCategory")
+        public String index1( Model model) {
+            return "layout/list_categories";
+        }
+
+
+
+
+}
