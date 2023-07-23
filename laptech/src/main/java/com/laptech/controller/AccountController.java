93c48a517e2b4ae25d548f8c57a5f047383a4661
phungtuhieu <phung.tuhieu@gmail.com>
2023-07-23 22:40:56 +0700

    [Link css] - Tự Hiếu, sửa lỗi

--- laptech/src/main/java/com/laptech/controller/AccountController.java
+++ laptech/src/main/java/com/laptech/controller/AccountController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/account")
 public class AccountController {
 
     @RequestMapping("/login")
