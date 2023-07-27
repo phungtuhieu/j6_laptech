668ab53bfb1d11cd46c3b7cae216ebdd2499caee
HuuTaiOne <nguyentaint979@gmail.com>
2023-07-27 16:04:34 +0700

    du phong

--- laptech/src/main/java/com/laptech/dao/CategoryDAO.java
+++ laptech/src/main/java/com/laptech/dao/CategoryDAO.java
@@ -1,9 +1,13 @@
 package com.laptech.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.laptech.model.Category;
 
 public interface CategoryDAO extends JpaRepository<Category,Long> {
+
+    List<Category> findByNameLike(String name);
     
 }
--- laptech/src/main/resources/static/admin/assets/js/angularjs/category.js
+++ laptech/src/main/resources/static/admin/assets/js/angularjs/category.js
@@ -3,13 +3,13 @@ var i = 0;
 const app = angular.module("app", []);
 app.controller("list", list).
     controller("create", formCreate).
-    controller("update", formUpdate);
+    controller("update", formUpdate).
+    controller("excel",excel);
 
 function list($scope, $http) {//
   $scope.form = {};
   $scope.items = [];  
   $scope.pageCount;
-
   $scope.reset = () => {
     $scope.form = {};
   };
@@ -28,7 +28,6 @@ function list($scope, $http) {//
         console.log("Error", error);
       });
   };
-
   // sap xep
   $scope.reverseSort = false;
   $scope.sortColumn = 'id';
@@ -37,38 +36,6 @@ function list($scope, $http) {//
   $scope.reverseSort = ($scope.sortColumn === prop) ? !$scope.reverseSort : false;
   $scope.sortColumn = prop;
   };
-  $scope.formEdit = (id) => {
-    // Lưu id vào session
-    window.sessionStorage.setItem("editId", id)  
-  };
-  $scope.delete = (id) => {
-    var url = `${host}/category/${id}`;
-    //var url = host+'/students.json';
-    $http({
-      method: "delete",
-      url: url,
-    })
-      .then((resp) => {
-        var index = $scope.items.findIndex(
-          (item) => item.id == $scope.form.id
-        );
-        $scope.items.splice(index, 1);
-        $scope.reset();
-        console.log("Success", resp);
-      
-      })
-      .catch((error) => {
-        console.log("Error", error);
-      });
-  };
-
-
-
-
-
-  // thực hiện
-  $scope.reset();
-  $scope.load_all();
 
   $scope.begin = 0;
   $scope.pageCount = Math.ceil($scope.items.length / 5);
@@ -88,6 +55,60 @@ function list($scope, $http) {//
 	$scope.last = function () {
 		$scope.begin = ($scope.pageCount - 1) * 5;
 	}
+// 
+  $scope.formEdit = (id) => {
+    // Lưu id vào session
+    window.sessionStorage.setItem("editId", id)  
+  };
+  $scope.delete = (id) => {
+    var url = `${host}/category/${id}`;
+    //var url = host+'/students.json';
+    $http({
+      method: "delete",
+      url: url,
+    })
+      .then((resp) => {
+        var index = $scope.items.findIndex(
+          (item) => item.id == id
+        );
+        $scope.items.splice(index, 1);
+        $scope.reset();
+        console.log("Success", resp);
+        $scope.pageCount = Math.ceil($scope.items.length / 5);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+        if (error.status === 422) {
+          alert("Danh mục đã tồn tại trong sản phẩm. Cập nhật không thành công.");
+        } else {
+          alert("Đã xảy ra lỗi khi cập nhật danh mục.");
+        }
+      });
+  };
+// 
+  $scope.search = (name) =>{
+    if(name !=""){
+      var url = `${host}/category/search/${name}`;
+    }else{
+      var url = `${host}/category`;
+    }
+    $http({
+      method: "GET",
+      url: url,
+    })
+      .then((resp) => {
+        $scope.items = resp.data;
+        $scope.pageCount = Math.ceil($scope.items.length / 5);
+        console.log("search", resp);
+      })
+      .catch((error) => {
+        console.log("Error_edit", error);
+      });
+  }
+  // thực hiện
+  $scope.reset();
+  $scope.load_all();
+  
 }
 function formCreate($scope, $http){ //
   $scope.form = {};
@@ -176,3 +197,84 @@ function formUpdate($scope, $http){ //
   const id = window.sessionStorage.getItem("editId");
   $scope.edit(id)
 }
+function excel($scope,$http){ //
+  // Excel
+  $scope.import = (files) => {
+    var confirmImport = confirm("Bạn có muốn import file Excel?");
+    if (confirmImport) {
+      var reader = new FileReader();
+      reader.onloadend = async () => {
+        var workbook = new ExcelJS.Workbook();
+        await workbook.xlsx.load(reader.result);
+        const worksheet = workbook.getWorksheet("category_data");
+        if (!worksheet) {
+          alert("Tên worksheet không đúng. Vui lòng sửa lại tên worksheet.");
+          return;
+        }    
+        worksheet.eachRow((row, index) => {
+          if (index > 1) {
+            let student = {
+              id: row.getCell(1).value,
+              name: row.getCell(2).value,
+              description: +row.getCell(3).value
+            };
+            let url = `${host}/category`;
+            $http.post(url, student)
+              .then(resp => {
+                console.log("Success", resp.data);
+              })
+              .catch(error => {
+                console.log("Error", error);
+              });
+
+              $scope.load_all();
+          }
+        });
+      };
+
+      reader.readAsArrayBuffer(files[0]);
+    }
+  };
+  // /Excel
+
+
+  // PDF
+  $scope.exportToPDF = function() {
+    var tableData = [];
+    var headers = ['ID', 'Tên danh mục', 'Mô tả'];
+
+    // Thêm dữ liệu của từng hàng (row) trong bảng vào mảng tableData
+    angular.forEach($scope.items, function(item) {
+        var rowData = [item.id, item.name, item.description];
+        tableData.push(rowData);
+    });
+
+    // Tạo nội dung PDF
+    var docDefinition = {
+      content: [
+          { text: 'Danh sách danh mục', style: 'header' },
+          {
+              table: {
+                  headerRows: 1,
+                  widths: ['auto', '*', '*'],
+                  body: [headers].concat(tableData)
+              },
+              style: 'table'
+          }
+      ],
+      styles: {
+          header: { fontSize: 20, bold: true, margin: [0, 0, 0, 10] },
+          table: { margin: [0, 5, 0, 15], fontSize: 12 },
+          tableHeader: { fillColor: '#FF0000', bold: true } // In đậm tiêu đề
+      }
+  };
+
+    // Xuất PDF
+    pdfMake.createPdf(docDefinition).download('danh_muc.pdf');
+};
+  // /PDF
+
+}
+
+
+
